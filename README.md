74342f694eb446655ce1cbb243cc72a403a2ace6
Chandan Prasad <cprasad@ariqt.com>
2021-10-17 13:45:58 +0530

    Added the python runbook for cost analysis.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# automation-azure-costmgmt
+# Azure Cost Management Runbook
+
+A Python script (Runbook) for getting the daily cost of azure resources in subscription level.
+
+## Prerequiste
+
+- Python 3.7+
+- Azure Account
+- Any code editor. (Preferable: [VS Code](https://code.visualstudio.com/download))
+
+## Python packages
+
+| Package | Uses |
+| ------ | ------ |
+| [Adal](https://pypi.org/project/adal/) or [MSAL](https://pypi.org/project/msal/) | The Microsoft Authentication Library for Python enables applications to integrate with the Microsoft identity platform. [PlDb] |
+| [Requests](https://pypi.org/project/requests/) | Requests is a simple, yet elegant, HTTP library. [PlGh] |
+| [AutomationAssets](https://pypi.org/project/azure-mgmt-automation/) | This is the Microsoft Azure Automation Client Library. [PlGd] |
+| [Pandas](https://pypi.org/project/pandas/) | Pandas is a Python package that provides fast, flexible, and expressive data structures designed to make working with "relational" or      "labeled" data both easy and intuitive. [PlOd] |
+| [Pretty_html_table](https://pypi.org/project/pretty-html-table/) | pretty_html_table exists to convert a pandas DataFrame into a pretty html table for use in email. The intended target audience is anyone who needs to send reports via email and would like to make their tables look more attractive. [PlMe] |
+| SMTPLib | Smtp Lib is a client library which will help you in sending the mail. [PlGa] |
+
+## Insallation
+
+- Install all the above mentioned packages through `pip install <package-name>`.
+- Login to [Azure](https://portal.azure.com/#home) account.
+- Search `Cost Management + Billing` and navigate to `Cost Management` > `Cost analysis` and, then set filter based on your requirement.
+
+## Run Script
+
+- Run the `azureCostMgmt.py` to get the daily usage data. 
+
+# Blog
+
+You can checkout my [blog](https://medium.com/@cprasad1512) for more information about this.
---
+++ azureCostMgmt.py
@@ -0,0 +1,191 @@
+import json
+import adal
+import requests
+import automationassets
+import pandas as pd
+from pretty_html_table import build_table
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import date, timedelta
+
+class MyUsage:
+
+
+    ######################### Getting the login OAuth Token #################################################
+
+    def __init__(self):
+        runas_connection = automationassets.get_automation_connection("AzureRunAsConnection")
+        tenant_id = runas_connection["TenantId"]
+        subscription_id = runas_connection["SubscriptionId"]
+        scope = "subscriptions/" + subscription_id
+        self.costmanagementUrl = "https://management.azure.com/" + scope + \
+            "/providers/Microsoft.CostManagement/query?api-version=2019-11-01&$top=4000"
+        authority_uri = automationassets.get_automation_variable("activeDirectoryEndpointUrl") + \
+            "/" + tenant_id
+        context = adal.AuthenticationContext(authority_uri)
+        token = context.acquire_token_with_client_credentials(
+            automationassets.get_automation_variable("resourceManagerEndpointUrl"),
+            automationassets.get_automation_variable("clientId"),
+            automationassets.get_automation_variable("clientSecret"))
+        bearer = "bearer " + token.get("accessToken")
+        self.headers = {"Authorization": bearer,
+                        "Content-Type": "application/json"}
+        self.usagedata = []
+
+
+    ########################## Getting the Usage data of your resources ###########################################################        
+
+    def run(self, startdate, enddate, grain="Daily", groupby = None):
+
+        payload = {
+            "type": "ActualCost",
+            "dataSet": {
+                    "granularity": grain,
+                    "aggregation": {
+                        "totalCost": {
+                            "name": "PreTaxCost",
+                            "function": "Sum"
+                        },
+                        "totalCostUSD": {
+                            "name": "PreTaxCostUSD",
+                            "function": "Sum"
+                        }
+                    }
+            },
+            "timeframe": "Custom",
+            "timePeriod": {
+                "from": startdate,
+                "to": enddate
+            }
+        }
+
+        if groupby != None:
+            payload['dataSet']['grouping'] = [{
+                "type": "Dimension",
+                "name": groupby
+            }]
+
+        payloadjson = json.dumps(payload)
+        print("Payload Json: ", payloadjson)
+        self.usagedata = []
+        response = requests.post(
+            self.costmanagementUrl, data=payloadjson, headers=self.headers)
+        if response.status_code == 200:
+            self.transform(payloadjson, response.text)
+        else:
+            print("error")
+            print("error " + response.text)
+
+        return self.usagedata
+
+    ######################## Tranforming the response to a data set #####################################
+
+    def transform(self, payloadjson, response):
+        result = json.loads(response)
+        print("Result: ", result)
+        for record in result["properties"]["rows"]:
+            usageRecord = {}
+            for index, val in enumerate(record):
+                columnName = result["properties"]["columns"][index]
+                if columnName["type"] == "Number":
+                    usageRecord[columnName["name"]] = val
+                else:
+                    usageRecord[columnName["name"]] = val
+
+            self.usagedata.append(usageRecord)
+
+        nextLink = result["properties"]["nextLink"]
+        if nextLink != None:
+            nextLinkResponse = requests.post(
+                nextLink, data=payloadjson, headers=self.headers)
+            if nextLinkResponse.status_code == 200:
+                self.transform(payloadjson, nextLinkResponse.text)
+            else:
+                print("error in fetching next page " + nextLink)
+                print("error " + nextLinkResponse.text)
+
+
+    ############################ Making the dataframe based on Resource group ###############################
+
+    def makeResourceWiseTable(self,usageData):
+        data=usageData
+        df=pd.DataFrame(data)
+        df.sort_values('ResourceGroup', inplace=True)
+        df.drop_duplicates(subset='ResourceGroup', keep='first', inplace=True)
+
+        return df
+    
+
+    ################################## Making the dataframe based on Total Cost ###############################################
+ 
+    def makeTotalCostTable(self, resourceWiseCostData):
+        resourceCostData=resourceWiseCostData
+        column_sum = resourceCostData['PreTaxCost'].sum()
+
+        cost=[]
+        totalCost=[column_sum]
+        print(type(totalCost))
+        for item in totalCost:
+            cost.append(item)
+        
+        df = pd.DataFrame(cost, columns=['TotalCost'])
+
+        return df
+
+
+    ################################### Sending mail of Daily Usage Report ##################################################
+
+    def send_mail(self, body1, body2):
+        message = MIMEMultipart()
+        sender = automationassets.get_automation_variable("FromMail")
+        receivers = ['xxx@mycompany.com','xxx@mycompany.com','xxx@mycompany.com','xxx@mycompany.com','xxx@mycompany.com']
+        # receivers = 'xxx.mycompany.com'
+        message['Subject'] = 'Daily Cost Analysis Report'
+        message['From'] = sender
+        message['To'] = ";".join(receivers)
+        # message['To'] = sender
+        body_content = "<h3>Date: <font color=#33cc33>"+convertToStrpday+" <font color=#000000>to ""<font color=#33cc33>"+convertToStrtday+"</h3><h3><font color=#000000>Subscription: <font color=#0099ff><SubscriptionNamw></h3><h2><font color=#000000>Total Cost of Single Day Usage:</h2>"+body2+"\n"+"<h2><font color=#000000>Cost of Individual Resource Group:</h2>"+body1
+
+        message.attach(MIMEText(body_content,"html"))
+        msg_body = message.as_string()
+
+        smtpObj = smtplib.SMTP('yourcompany.com',25)
+        smtpObj.sendmail(sender, receivers, msg_body)
+
+
+    ######################################## Making the attaractive table ############################################
+
+    def send_report(self, usageData):
+        tabulate_form1 = self.makeResourceWiseTable(usageData)
+        tabulate_form2 = self.makeTotalCostTable(tabulate_form1)
+        output1 = build_table(tabulate_form1, 'yellow_dark')
+        output2 = build_table(tabulate_form2, 'green_dark')
+        try:
+            self.send_mail(output1, output2)
+            print('Mail sent successfullly!!')
+        except:
+            print('Unable to send Mail')
+        
+
+
+def run_example():
+    azure_usage = MyUsage()
+    usageResult = azure_usage.run("2021-09-27", "2021-09-28")
+    usageResult = azure_usage.run(
+        convertToStrpday, convertToStrtday, groupby="ResourceGroup")
+    print(*usageResult, sep="\n")
+    azure_usage.send_report(usageResult)
+    print("Done")
+
+
+################################## Getting the Yesterday and Today's date ####################################
+ 
+today = date.today()
+previuosDate = today-timedelta(days=1)
+convertToStrtday=today.strftime("%Y-%m-%d")
+convertToStrpday=previuosDate.strftime("%Y-%m-%d")
+
+
+if __name__ == "__main__":
+    run_example()
